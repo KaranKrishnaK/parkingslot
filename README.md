b5b3258e2d71a20b6a2c69b324771fe3f25ac0b8
KaranKrishnaK <kkrishna@mcfadyen.com>
2022-11-15 06:59:56 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+#How to use this repository
+
+Install all dependencies (testing dependent packages)
+"npm install"
+
+Run the main program
+"node index.js"
---
+++ index.js
@@ -0,0 +1,73 @@
+const readline = require("readline");
+const parkingLotInstance = require("./parkingLot");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+/**
+ * Parser for the commands that user input
+ */
+let intiMain = () => {
+  rl.on("line", async (input) => {
+    input = input.split(" ");
+    switch (input[0]) {
+      case "create":
+        try {
+          const result = await parkingLotInstance.createParkingLot(input[1]);
+          console.log(result);
+        } catch (e) {
+          console.log(`error occured ==> ${e}`);
+        }
+        break;
+
+      case "park":
+        // console.log("input", input);
+        try {
+          const result = await parkingLotInstance.park(
+            input[1].trim(),
+            input[2].trim()
+          );
+          console.log(result);
+        } catch (e) {
+          console.log(`error occured ==> ${e}`);
+        }
+        break;
+
+      case "leave":
+        try {
+          const result = await parkingLotInstance.leave(input[1]);
+          console.log(result);
+        } catch (e) {
+          console.log(`error occured ==> ${e}`);
+        }
+        break;
+
+      case "status":
+        try {
+          const result = await parkingLotInstance.status();
+          console.log(result);
+        } catch (e) {
+          console.log(`error occured ==> ${e}`);
+        }
+        break;
+
+      case "exit":
+        rl.pause();
+        break;
+
+      default:
+        console.log(
+          "Seems like an issue with command that you typed , please note predeifed commands are case sensitive and matched as per the description!"
+        );
+    }
+  });
+};
+
+rl.on("SIGINT", () => {
+  rl.question("Are you sure you want to exit? (yes/no) ", (answer) => {
+    if (answer.match(/^y(es)?$/i)) rl.pause();
+  });
+});
+
+intiMain();
